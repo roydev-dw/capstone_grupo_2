46753605d6f631ca6836b8a555a83e59528661c9
roydev-dw <ado.pezzini@gmail.com>
2025-09-24 19:15:20 -0300

    estructura base front

--- foodTruckApp/frontend/vite.config.js
+++ foodTruckApp/frontend/vite.config.js
@@ -1,8 +1,43 @@
 import { defineConfig } from 'vite';
-import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: [
+        'favicon.svg',
+        'favicon.ico',
+        'robots.txt',
+        'apple-touch-icon.png',
+      ],
+      manifest: {
+        name: 'Mi App React PWA',
+        short_name: 'MiApp',
+        description: 'Ejemplo de PWA con React y Vite',
+        theme_color: '#0f766e',
+        background_color: '#ffffff',
+        display: 'standalone',
+        scope: '/',
+        start_url: '/',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
 });
