138233a901fbf65fe5712f1ff8d43158ea21e178
Vicente Gálvez Roldán <vincent@MacBook-Air-de-Vincent.local>
2025-10-01 13:48:10 -0300

    Subida Backend

---
+++ foodTruckApp/backend/core/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("home")
+        else:
+            return render(request, "login/login.html", {"error": "Usuario o contraseña inválidos"})
+    return render(request, "login/login.html")
+
+
+@login_required
+def home_view(request):
+    return render(request, "base/home.html", {"user": request.user})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect("login")
